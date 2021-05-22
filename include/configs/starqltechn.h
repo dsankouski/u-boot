2602a7e98a05915c61c7c3386fcffbfeca664716
dzmitry <dsankouski@gmail.com>
2021-05-22 17:44:16 +0300

    add muic manual switch config

--- include/configs/starqltechn.h
+++ include/configs/starqltechn.h
@@ -38,7 +38,7 @@
 #define BOOT_TARGET_DEVICES(func) \
 	func(MMC, mmc, 0)
 
-#define CONFIG_EXTRA_ENV_SETTINGS \
+#define EXTRA_ENV_SETTINGS \
 	"loadaddr=0x80000000\0" \
 	"fdt_high=0xffffffffffffffff\0" \
 	"initrd_high=0xffffffffffffffff\0" \
@@ -49,6 +49,54 @@
 	"ramdisk_addr_r=0x91000000\0"\
 	"scriptaddr=0x90000000\0"\
 	"pxefile_addr_r=0x90100000\0"
+
+#ifdef CONFIG_MUIC_MANUAL_SWITCH
+#define MUIC_DEFINITIONS \
+"I2C_MUIC_BUS_NUM=0\0" \
+"I2C_MUIC_ADDR=0x66\0" \
+"COM_UART=3\0" \
+"S2MU004_REG_MUIC_SW_CTRL=0xCA\0" \
+"MANUAL_SW_UART_VBUS_CLOSED=0x48\0" \
+"MANUAL_SW_OPEN=0x00\0" \
+"S2MU004_REG_MUIC_CTRL1=0xC7\0" \
+"MUIC_CTRL1_RAW_DATA_MAN_SWITCH=0x12\0" \
+"MUIC_CTRL1_INITIAL=0x17\0" \
+"BYTE_COUNT=1\0"
+
+#define MUIC_CONNECT_UART \
+    "connect_uart_usb=" \
+    "echo switching i2c pins to gpio mode; " \
+    "mw 0x3921000 0 1" \
+    "mw 0x3922000 0 1" \
+    "echo connecting uart to usb signal lines...; " \
+    "echo uart connected to usb signal lines;" \
+    "echo CAUTON! Do NOT connect USB host before disconnecting uart ('run disconnect_uart_usb'), this will damage your device;\0"
+
+#define MUIC_DISCONNECT_UART \
+    "disconnect_uart_usb=" \
+    "echo disconnecting uart from usb signal lines...; " \
+    "echo uart disconnected from usb signal lines;\0"
+
+#define CONFIG_EXTRA_ENV_SETTINGS \
+    EXTRA_ENV_SETTINGS \
+    MUIC_DEFINITIONS \
+	MUIC_DISCONNECT_UART \
+	MUIC_CONNECT_UART
+
+#define CONFIG_PREBOOT \
+"echo Check pressed buttons match uart on usb combination...;" \
+"PRESSED=1;" \
+"RELEASED=0;" \
+"if test " \
+"$key_vol_down -eq $PRESSED; " \
+"then run connect_uart_usb; else " \
+"echo pressed buttons does not match;" \
+"fi;"
+
+#else
+#define CONFIG_EXTRA_ENV_SETTINGS EXTRA_ENV_SETTINGS
+#endif
+
 
 /* Size of malloc() pool */
 #define CONFIG_SYS_MALLOC_LEN		(CONFIG_ENV_SIZE + SZ_8M)
